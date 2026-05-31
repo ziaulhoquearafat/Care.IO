08aa15de974b790c6d81c3f7d1958fc992d99ef1
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-06-01 05:54:12 +0600

    setup project environment and install shancnui and project folder skelton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Noto_Sans, Montserrat } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const montserratHeading = Montserrat({subsets:['latin'],variable:'--font-heading'});
+
+const notoSans = Noto_Sans({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", notoSans.variable, montserratHeading.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>

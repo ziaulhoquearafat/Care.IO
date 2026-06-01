777600fe35839b6a3bb217026a8d1e62d2500a17
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-06-01 06:21:44 +0600

    add NotFound component and form components for booking, login, registration, and home sections

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <Button>Go Home</Button>
+    </div>
+  );
+}
